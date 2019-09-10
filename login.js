46dea74f3b798481a2375656347f2cf34e59667b
SheronSuditha <github@sudithakumarasinghe.me>
2019-09-10 12:50:50 +1000

    Uninstalled node-fetch Installed request
    npm install request --save

--- login.js
+++ login.js
@@ -1,7 +1,23 @@
-
+const request = require('request');
 function loginRequest() {
     if (document.getElementById('password').value == 'admin') {
         const { ipcRenderer } = require('electron');
         ipcRenderer.send('newWindow', 'profile.html');
     }
+}
+
+
+const body = { a : 1 };
+
+function doFetch() {
+    var url =  "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json";
+    fetch(url, {
+        method: 'GET',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(res => res.json())
+    .then(json => console.log());
 }
